41bd5fb222bde7af2ee216d2aee062852b257fd6
erdal burak cinar <erdalburakcinar@gmail.com>
2023-07-17 17:44:21 +0300

    Initial Commit

---
+++ DeepFit/DeepFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepFitApp.swift
+//  DeepFit
+//
+//  Created by Erdal Burak Cinar on 17.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct DeepFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
